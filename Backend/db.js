2e74638a0a2b5fb33ff9299414e57151ab0ffaee
NaufalRI <60543742+rakhaizzandi@users.noreply.github.com>
2026-04-27 14:53:00 +0700

    Delete Backend directory

--- Backend/db.js
+++
@@ -1,7 +0,0 @@
-const { neon } = require('@neondatabase/serverless');
-require('dotenv').config();
-
-// Create SQL client for Vercel Postgres/Neon
-const sql = neon(process.env.POSTGRES_URL || process.env.DATABASE_URL);
-
-module.exports = sql;
